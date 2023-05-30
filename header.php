a0a17832e88c82524638b80d85fdebc6270c7c3d
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2023-05-30 10:03:35 +0100

    colour/logo/font change

--- header.php
+++ header.php
@@ -19,16 +19,10 @@ session_start();
         charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1">
     <link rel="preload"
-        href="<?=get_stylesheet_directory_uri()?>/fonts/poppins-v20-latin-300.woff2"
+        href="<?=get_stylesheet_directory_uri()?>/fonts/montserrat-v25-latin-700.woff2"
         as="font" type="font/woff2" crossorigin="anonymous">
     <link rel="preload"
-        href="<?=get_stylesheet_directory_uri()?>/fonts/poppins-v20-latin-500.woff2"
-        as="font" type="font/woff2" crossorigin="anonymous">
-    <link rel="preload"
-        href="<?=get_stylesheet_directory_uri()?>/fonts/poppins-v20-latin-600.woff2"
-        as="font" type="font/woff2" crossorigin="anonymous">
-    <link rel="preload"
-        href="<?=get_stylesheet_directory_uri()?>/fonts/poppins-v20-latin-700.woff2"
+        href="<?=get_stylesheet_directory_uri()?>/fonts/montserrat-v25-latin-regular.woff2"
         as="font" type="font/woff2" crossorigin="anonymous">
     <?php
 if (get_field('ga_property', 'options')) {
@@ -88,7 +82,7 @@ wp_head();
         {
             "@context": "http://schema.org",
             "@type": "Organization",
-            "name": "Gaffsy",
+            "name": "NEPB",
             "url": "https://www.nepropertybuyers.co.uk/",
             "logo": "https://www.nepropertybuyers.co.uk/wp-content/theme/cb-nepb/img/nepb-logo.jpg",
             "description": "Sell House Fast | Quick House Sale For Cash | NE Property Buyers",
